7cc7108da395437d31fb153e5688d4dcb507e713
fiam <fiam@users.noreply.github.com>
2025-11-04 22:03:06 +0000

    feat: add config seeding for homeassistant

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -1,29 +1,48 @@
 locals {
   home_assistant_external_hostname = "${var.cloudflare_homeassistant_subdomain}.${var.cloudflare_domain}"
 }
-  
+
 resource "cloudflare_zero_trust_tunnel_cloudflared" "_" {
-    account_id   = var.cloudflare_account_id
-    name         = var.cloudflare_tunnel_name
-    config_src   = "cloudflare"
+  account_id = var.cloudflare_account_id
+  name       = var.cloudflare_tunnel_name
+  config_src = "cloudflare"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "_" {
-  account_id   = var.cloudflare_account_id
-  tunnel_id   = cloudflare_zero_trust_tunnel_cloudflared._.id
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared._.id
   config = {
     ingress = [{
       hostname = local.home_assistant_external_hostname
-      service = "http://${local.helm_release_name}-homeassistant.default.svc.cluster.local"
-    },
-    {
-        service  = "http_status:404"
+      service  = "http://${local.helm_release_name}-homeassistant.default.svc.cluster.local"
+      },
+      {
+        service = "http_status:404"
     }]
   }
-} 
+}
 
 data "cloudflare_zero_trust_tunnel_cloudflared_token" "_" {
-  account_id   = var.cloudflare_account_id
-  tunnel_id   = cloudflare_zero_trust_tunnel_cloudflared._.id
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared._.id
+}
+
+data "cloudflare_zone" "_" {
+  filter = {
+    account = {
+      id = var.cloudflare_account_id
+    }
+    name  = var.cloudflare_domain
+    match = "all"
+  }
+}
+
+resource "cloudflare_dns_record" "_" {
+  zone_id = data.cloudflare_zone._.zone_id
+  name    = var.cloudflare_homeassistant_subdomain
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared._.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
 }
 
--- infra/helm.tf
+++ infra/helm.tf
@@ -1,23 +1,23 @@
 locals {
-    helm_release_name = "app"
+  helm_release_name = "app"
 }
 resource "helm_release" "_" {
-    depends_on = [ kubernetes_secret.cloudflared_tunnel_token_secret ]
-    namespace = var.kubernetes_namespace
-    name = local.helm_release_name
-    chart = "../domotic"
-#    atomic = true
-    create_namespace = true
-    cleanup_on_fail = true
+  depends_on = [kubernetes_secret.cloudflared_tunnel_token_secret]
+  namespace  = var.kubernetes_namespace
+  name       = local.helm_release_name
+  chart      = "../domotic"
+  #    atomic = true
+  create_namespace = true
+  cleanup_on_fail  = true
 
-    set = [
-        {
-            name = "homeassistant.config.external_url",
-            value = "https://${local.home_assistant_external_hostname}"
-        },
-        {
-            name = "zigbee2mqtt.config.serial.port",
-            value = var.serial_port
-        }
-    ]
+  set = [
+    {
+      name  = "homeassistant.config.external_url",
+      value = "https://${local.home_assistant_external_hostname}"
+    },
+    {
+      name  = "zigbee2mqtt.config.serial.port",
+      value = var.serial_port
+    }
+  ]
 }
