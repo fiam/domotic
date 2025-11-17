ba5710f75706abaaf923fcda3933389180062d6a
fiam <fiam@users.noreply.github.com>
2025-11-17 12:29:44 +0000

    feat: simplify umbrella chart

--- kind-create.sh
+++ kind-create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat <<EOF | kind create cluster --config=-
+cat <<EOF | kind create cluster --name domotic --config=-
 apiVersion: kind.x-k8s.io/v1alpha4
 kind: Cluster
 nodes:
@@ -11,14 +11,24 @@ nodes:
         protocol: TCP
 EOF
 
+# Install Gateway API CRDs
 kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.1.4" | kubectl apply -f -
-helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway --set nginx.service.type=NodePort --set-json 'nginx.service.nodePorts=[{"port":31437,"listenerPort":80}]'
 
+# Install nginx-gateway-fabric controller
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric \
+  --create-namespace \
+  -n nginx-gateway \
+  --set nginx.service.type=NodePort \
+  --set-json 'nginx.service.nodePorts=[{"port":31437,"listenerPort":80}]'
+
+# Create Gateway resource (cluster-level infrastructure)
+# This Gateway accepts HTTP traffic from all namespaces
 cat <<EOF | kubectl apply -f -
 apiVersion: gateway.networking.k8s.io/v1
 kind: Gateway
 metadata:
   name: gateway
+  namespace: default
 spec:
   gatewayClassName: nginx
   listeners:
@@ -28,9 +38,7 @@ spec:
     allowedRoutes:
       namespaces:
         from: All
-# Omitting the hostname means this gateway will accept all hostnames
-#    hostname: "*.example.com"
 EOF
 
-
+# Install zstackmulator (Zigbee emulator for development)
 kubectl apply -k zstackmulator
